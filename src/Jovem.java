17c53521dcdd23dae2fb1aaba80e587cc44285de
Gabriel Dias <gabrielmdais557@gmail.com>
2024-05-31 21:31:13 -0300

    commit inicial com novo repositório de java

---
+++ src/Jovem.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Jovem {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Digite a sua idade");
+        int idade = entrada.nextInt();
+
+        if (idade >=25){
+            System.out.println("Você é velho!");
+        } else if (idade == 24) {
+            System.out.println("Você é gay!kkk");
+        } else{
+            System.out.println("Você é jovem!");
+        }
+
+    }
+}
