81cbb30c4e0fc57a60123e1484f5bd46d7900b37
Gabriel Dias <gabrielmdais557@gmail.com>
2024-06-02 15:17:45 -0300

    AreaCirculo

---
+++ src/AreaCirculo.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class AreaCirculo {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Digite o raio do seu círculo: ");
+        double raio = Double.parseDouble(entrada.nextLine());
+    }
+}
