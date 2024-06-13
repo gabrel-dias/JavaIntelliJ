8559644ecea90edcae443566ca320fcd223973eb
Gabriel Dias <gabrielmdais557@gmail.com>
2024-06-13 02:29:58 -0300

    que aberração é essa

--- src/stream_API/ConsumerTeste.java
+++ src/stream_API/ConsumerTeste.java
@@ -2,17 +2,15 @@ package stream_API;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.function.Consumer;
 
 public class ConsumerTeste {
     public static void main(String[] args) {
         List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5);
 
-        Consumer<Integer> imprimirPar = numero -> {
+        numeros.forEach(numero -> {
             if (numero % 2 == 0) {
                 System.out.println(numero);
             }
-        };
-        numeros.forEach(imprimirPar);
+        });
     }
 }
