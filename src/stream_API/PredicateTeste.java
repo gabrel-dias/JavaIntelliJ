80bd61b5c1c69384fd855685f9baaec4a36bf9bd
Gabriel Dias <gabrielmdais557@gmail.com>
2024-06-13 23:06:44 -0300

    predicate tá sendo mais fácil de eu entender eu acho

---
+++ src/stream_API/PredicateTeste.java
@@ -0,0 +1,20 @@
+package stream_API;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
+/**
+ * Representa uma função que aceita um argumento do tipo T e retorna um valor boolean.
+ * Comumente usada para filtrar os elementos do Stream com base em alguma condição.
+ */
+public class PredicateTeste {
+    public static void main(String[] args) {
+        List<String> palavras = Arrays.asList("java", "python", "c++", "c#", "javascript");
+
+        // não é preciso especificar a saída do Predicate pois ela sempre será um boolean
+        Predicate<String> maisDeCinco = palavra -> palavra.length() > 5;
+
+        palavras.stream().filter(maisDeCinco).forEach(System.out::println);
+    }
+}
