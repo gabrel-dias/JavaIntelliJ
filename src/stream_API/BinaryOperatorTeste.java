c36db3552eb046545bfb1de47ba8e285b9c760e0
Gabriel Dias <gabrielmdais557@gmail.com>
2024-06-14 03:04:00 -0300

    binary muito interessante de aprender também, acho que não sou um fracassado no fim das contas

---
+++ src/stream_API/BinaryOperatorTeste.java
@@ -0,0 +1,21 @@
+package stream_API;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Representa uma operação que combina dois argumentos do tipo T e retorna um resultado do mesmo tipo T.
+ * É usada para realizar operações de redução em pares de elementos, como somar números ou combinar objetos.
+ */
+public class BinaryOperatorTeste {
+    public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(1,2,3,4,5);
+
+//        BinaryOperator<Integer> somar = (num1, num2) -> num1+num2;
+
+        int resultado = numeros.stream()
+                .reduce(0, Integer::sum);
+
+        System.out.println(resultado);
+    }
+}
