f30e0743692593410c532352b834f70d4a8df820
Gabriel Dias <gabrielmdais557@gmail.com>
2024-06-28 22:23:11 -0300

    arrumações nas contas bancárias

--- src/desafios/AbrindoContas.java
+++ src/desafios/AbrindoContas.java
@@ -6,6 +6,7 @@ public class AbrindoContas {
 
     public static void main(String[] args) {
         // Lendo os dados de Entrada:
+
         Scanner scanner = new Scanner(System.in);
         int numeroConta = scanner.nextInt();
         scanner.nextLine(); // Consome a quebra de linha após a entrada do número da conta
@@ -13,9 +14,12 @@ public class AbrindoContas {
         double saldo = scanner.nextDouble();
 
         //TODO: Criar uma instância de "ContaBancaria" com os valores de Entrada.
+        ContaBancaria conta = new ContaBancaria(numeroConta, nomeTitular, saldo);
 
-        System.out.println("Informações:");
+
+        System.out.println("Informacoes:");
         //TODO: Imprimir as informações da conta usando o objeto criado no TODO acima.
+        System.out.println(conta);
     }
 }
 
@@ -24,7 +28,7 @@ class ContaBancaria {
     String titular;
     double saldo;
 
-    public Bancaria(int numero, String titular, double saldo) {
+    public ContaBancaria(int numero, String titular, double saldo) {
         this.numero = numero;
         this.titular = titular;
         this.saldo = saldo;
