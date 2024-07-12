6080cce990fef3f58093ac8fde720a97ce184915
Gabriel Dias <gabrielmdais557@gmail.com>
2024-07-12 02:29:47 -0300

    HomensMulheres.java

---
+++ src/exercicios/HomensMulheres.java
@@ -0,0 +1,19 @@
+/*
+2. Tem-se um conjunto de dados contendo a altura e o sexo (masculino, feminino) de 10 pessoas.
+Fazer um algoritmo que calcule e escreva:
+a) a maior e a menor altura do grupo;
+b) média de altura dos homens;
+c) o número de mulheres.
+*/
+package exercicios;
+
+import java.util.Scanner;
+
+public class HomensMulheres {
+    public static void main(String[] args) {
+        int sexo, qtHomens, qtMulheres;
+        double media, alturaHomem, alturaMulher, maiorAltura, menorAltura;
+
+        Scanner entrada = new Scanner(System.in);
+    }
+}
