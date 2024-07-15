3e27efbc3c025374ac3f3f27e2281d6fa41f01d9
Gabriel Dias <gabrielmdais557@gmail.com>
2024-07-15 10:39:19 -0300

    testes idioma

--- src/exercicios/javaguanabara/IdiomaSistema.java
+++ src/exercicios/javaguanabara/IdiomaSistema.java
@@ -7,5 +7,8 @@ public class IdiomaSistema {
         Locale local = Locale.getDefault(); // pega os valores padrões do sistema no qual a JVM está instalada
         System.out.println("O país do usuário é: " + local.getDisplayCountry());
         System.out.println("O idioma do usuário é: " + local.getDisplayLanguage());
+
+        // testes
+        System.out.println(System.getProperty("user.language"));
     }
 }
