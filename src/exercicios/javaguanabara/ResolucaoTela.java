3e25b2b49ca4f119aa8084aa1869fcf204d718e2
Gabriel Dias <gabrielmdais557@gmail.com>
2024-07-16 02:54:25 -0300

    conclusão tela

--- src/exercicios/javaguanabara/ResolucaoTela.java
+++ src/exercicios/javaguanabara/ResolucaoTela.java
@@ -5,6 +5,6 @@ import java.awt.*;
 public class ResolucaoTela {
     public static void main(String[] args) {
         Dimension tela = Toolkit.getDefaultToolkit().getScreenSize();
-        System.out.println("A resolução do sistema é: " + tela);
+        System.out.println("A resolução do sistema é: " + (int) tela.getWidth() + "x" + (int) tela.getHeight());
     }
 }
