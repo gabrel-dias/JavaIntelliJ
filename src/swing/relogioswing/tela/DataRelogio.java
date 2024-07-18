4dbcd86ed2583e32bdf7e5f28b83d279d426ba51
Gabriel Dias <gabrielmdais557@gmail.com>
2024-07-18 02:55:51 -0300

    aprendendo relogio e calendario

---
+++ src/swing/relogioswing/tela/DataRelogio.java
@@ -0,0 +1,12 @@
+package swing.relogioswing.tela;
+
+import javax.swing.*;
+
+public class DataRelogio {
+    private JButton btnDataHora;
+    private JPanel panel1;
+
+    public static void main(String[] args) {
+
+    }
+}
