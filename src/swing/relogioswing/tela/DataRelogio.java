1e93a37accebbff849e255e425593ec6a8069a89
Gabriel Dias <gabrielmdais557@gmail.com>
2024-07-19 10:30:26 -0300

    mudanças no DataRelogio.java

--- src/swing/relogioswing/tela/DataRelogio.java
+++ src/swing/relogioswing/tela/DataRelogio.java
@@ -1,3 +1,4 @@
+// https://www.youtube.com/watch?v=whF_Qm1epQ8&t=267s
 package swing.relogioswing.tela;
 
 import javax.swing.*;
@@ -8,14 +9,14 @@ import java.util.Date;
 public class DataRelogio extends JFrame { // A CLASSE DEVE HERDAR O JFRAME
     // atributos que são gerados no painel .form
     private JButton btnDataHora;
-    private JPanel panel1;
+    private JPanel pnlDataHora;
     private JLabel lblHora;
 
     //construtor do painel
     public DataRelogio() {
         setTitle("Data e Hora"); // nome da janela
-        setSize(400, 200); // resolução da janela
-        setContentPane(panel1); // deixa os conteúdos do painel visíveis
+        setSize(300, 200); // resolução da janela
+        setContentPane(pnlDataHora); // deixa os conteúdos do painel visíveis
         setVisible(true); // deixa a janela visível
         setLocationRelativeTo(null); // na sua ausência, coloca a janela no canto da dela. com o valor null, no meio
         setDefaultCloseOperation(EXIT_ON_CLOSE); // escolhe uma ação que ocorre quando fechar a janela
@@ -25,7 +26,8 @@ public class DataRelogio extends JFrame { // A CLASSE DEVE HERDAR O JFRAME
             @Override
             public void actionPerformed(ActionEvent e) {
                 Date relogio = new Date();
-                lblHora.setText(relogio.toString());
+                JOptionPane.showMessageDialog(DataRelogio.this, relogio); // mostra uma nova janela
+                // de diálogo
             }
         });
     }
