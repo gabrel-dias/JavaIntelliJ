bfd8977e44bf19ca31a3b20b415d0c0c86610ff7
Gabriel Dias <gabrielmdais557@gmail.com>
2024-07-19 21:54:24 -0300

    gui IdiomaSistema.java

--- src/swing/idiomasistema/IdiomaSistema.java
+++ src/swing/idiomasistema/IdiomaSistema.java
@@ -1,20 +1,31 @@
 package swing.idiomasistema;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Locale;
 
 public class IdiomaSistema extends JFrame {
     private JLabel lblImagem;
     private JLabel lblIdiomaSistema;
     private JButton mostrarIdiomaButton;
     private JPanel pnlIdioma;
+    private JLabel lblIdioma;
 
     public IdiomaSistema() {
         setTitle("Idioma do sistema");
         setContentPane(pnlIdioma);
         setVisible(true);
-        setSize(450,200);
+        setSize(450, 200);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+        mostrarIdiomaButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Locale idioma = Locale.getDefault();
+                lblIdioma.setText("Seu idioma é o" + idioma.getDisplayLanguage());
+            }
+        });
     }
 
     public static void main(String[] args) {
