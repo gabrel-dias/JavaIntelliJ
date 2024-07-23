ca9db1fb52cfdf3f824c93b4b8854195372a319d
Gabriel Dias <gabrielmdais557@gmail.com>
2024-07-23 11:21:01 -0300

    SuperCalculadora.java 2

--- src/swing/supercalculadora/SuperCalculadora.java
+++ src/swing/supercalculadora/SuperCalculadora.java
@@ -6,4 +6,9 @@ public class SuperCalculadora extends JFrame {
     private JPanel pnlCalculadora;
     private JSpinner spnValor;
     private JButton btnCalcular;
+    private JLabel lblResto2;
+    private JLabel lblCubo;
+    private JLabel lblRaizQuadrada;
+    private JLabel lblRaizCubica;
+    private JLabel lblValorAbsoluto;
 }
