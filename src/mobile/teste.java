052150ae3198b961bdd3736ef24e7dd265c6bd60
Gabriel Dias <gabrielmdais557@gmail.com>
2024-08-19 08:48:08 -0300

    fazer método depois

---
+++ src/mobile/teste.java
@@ -0,0 +1,11 @@
+package mobile;
+
+public class teste {
+    public static void main(String[] args) {
+        System.out.println("Sem método");
+        imprimir();
+    }
+    public static void imprimir(){
+        System.out.println("Programa inicializado com sucesso");
+    }
+}
