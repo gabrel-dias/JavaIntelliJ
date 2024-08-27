6e65765f974e7f1d30470e78d5ed954a87fb36c5
Gabriel Dias <gabrielmdais557@gmail.com>
2024-08-27 16:42:02 -0300

    farenheit e celsius para projeto de ti

---
+++ src/FarenheitCelsius.java
@@ -0,0 +1,35 @@
+/*
+Faça um programa em Java que recebe uma temperatura em graus fahrenheit e transforme em graus celsius.
+Caso o resultado da temperatura seja maior ou igual a 40o celsius, mostre uma mensagem:
+ESTÁ QUENTE!, senão mostre TEMPERATURA FRIA OU SUPORTÁVEL.
+*/
+
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class FarenheitCelsius {
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        // variáveis que serão usadas
+        double c, f;
+
+        System.out.println("Digite uma temperatura em Farenheit que será convertida em Celsius:");
+        f = Double.parseDouble(entrada.nextLine());
+
+        // substituindo o separador decimal por uma vírgula e formatando as casas decimais do resultado
+        DecimalFormatSymbols virgula = new DecimalFormatSymbols(Locale.getDefault());
+        DecimalFormat arredondar = new DecimalFormat("#,##0.00", virgula);
+        c = (f - 32) / 1.8d;
+
+        System.out.println("A temperatura convertida em Celsius é: " + arredondar.format(c) + "°C");
+        // condicionais para classificar a temperatura
+        if (c >= 40) {
+            System.out.println("Está quente!");
+        } else {
+            System.out.println("Temperatura fria e suportável!");
+        }
+    }
+}
--- src/mobile/DisciplinaMobile4.java
+++ src/mobile/DisciplinaMobile4.java
@@ -1,19 +1,19 @@
-package mobile;
-
-import java.util.Scanner;
-
-public class DisciplinaMobile4 {
-    public static void main(String[] args) {
-        // TODO terminar depois
-imprimir();
-    }
-        public static String imprimir(String impressao){
-            Scanner teclado = new Scanner(System.in);
-        System.out.println("Digite algo: ");
-            String teclado = teclado.nextLine();
-            String resultado = imprimir(impressao);
-        // métodos precisam ser declarados fora do método main
-            String mensagem = impressao;
-            return mensagem;
-                    }
-}
+//package mobile;
+//
+//import java.util.Scanner;
+//
+//public class DisciplinaMobile4 {
+//    public static void main(String[] args) {
+//        // TODO terminar depois
+//imprimir();
+//    }
+//        public static String imprimir(String impressao){
+//            Scanner teclado = new Scanner(System.in);
+//        System.out.println("Digite algo: ");
+//            String teclado = teclado.nextLine();
+//            String resultado = imprimir(impressao);
+//        // métodos precisam ser declarados fora do método main
+//            String mensagem = impressao;
+//            return mensagem;
+//                    }
+//}
