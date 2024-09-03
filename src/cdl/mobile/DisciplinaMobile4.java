5bb3c60281df190647b50f1a64434d36a6c1b6e5
Gabriel Dias <gabrielmdais557@gmail.com>
2024-09-02 23:27:20 -0300

    método da DisciplinaMobile4.java arrumado só pra passsar o tempo

--- src/cdl/mobile/DisciplinaMobile4.java
+++ src/cdl/mobile/DisciplinaMobile4.java
@@ -1,19 +1,18 @@
-//package cdl.mobile;
-//
-//import java.util.Scanner;
-//
-//public class DisciplinaMobile4 {
-//    public static void main(String[] args) {
-//        // TODO terminar depois
-//imprimir();
-//    }
-//        public static String imprimir(String impressao){
-//            Scanner teclado = new Scanner(System.in);
-//        System.out.println("Digite algo: ");
-//            String teclado = teclado.nextLine();
-//            String resultado = imprimir(impressao);
-//        // métodos precisam ser declarados fora do método main
-//            String mensagem = impressao;
-//            return mensagem;
-//                    }
-//}
+package cdl.mobile;
+
+import java.util.Scanner;
+
+public class DisciplinaMobile4 {
+    public static void main(String[] args) {
+        imprimir();
+    }
+
+    public static void imprimir() {
+        System.out.println("Digite algo");
+        Scanner sc = new Scanner(System.in);
+        String algo = sc.nextLine();
+        sc.close();
+        System.out.println(algo);
+    }
+
+}
