aae755bd83ffe5152fa2a7d4f82c4b624e3e243c
Gabriel Dias <gabrielmdais557@gmail.com>
2024-09-02 23:29:33 -0300

    fórmula para calcular

--- src/cdl/mobile/CalculadoraIMC.java
+++ src/cdl/mobile/CalculadoraIMC.java
@@ -1,5 +1,8 @@
 package cdl.mobile;
-
+/*
+O índice de massa corporal (IMC) é calculado dividindo o peso em quilos pela altura em metros ao quadrado. A fórmula
+para calcular o IMC é: IMC = peso / (altura x altura).
+ */
 public class CalculadoraIMC {
     public static void main(String[] args) {
 
