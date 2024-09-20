b5af9c8d6e08ec4995735e350f0a7a92f9590d6f
Gabriel Dias <gabrielmdais557@gmail.com>
2024-09-20 09:27:30 -0300

    poundsKilo

--- src/udemy/FloatDouble.java
+++
@@ -1,7 +0,0 @@
-package udemy;
-
-public class FloatDouble {
-    public static void main(String[] args) {
-        float teste = 5.25f;
-    }
-}
---
+++ src/udemy/FloatDoubleChallenge.java
@@ -0,0 +1,9 @@
+package udemy;
+
+public class FloatDoubleChallenge {
+    public static void main(String[] args) {
+        double pounds = 200d;
+        double kilograms = pounds * 0.45359237d;
+        System.out.println(kilograms);
+    }
+}
