d3868319a7e04a6b555f9e3e6244180cdd4ae274
Gabriel Dias <gabrielmdais557@gmail.com>
2024-10-14 02:56:34 -0300

    construtores

--- src/poo/Main.java
+++ src/poo/Main.java
@@ -2,6 +2,8 @@ package poo;
 
 public class Main {
     public static void main(String[] args) {
+        Pessoa p1 = new Pessoa("Gabriel", "075", 24, "085");
+        p1.apresentar();
 
     }
 }
--- src/poo/Pessoa.java
+++ src/poo/Pessoa.java
@@ -6,4 +6,20 @@ public class Pessoa {
     private String cpf;
     private int idade;
     private String whatsapp;
+
+    // construtores servem para configurar o estado inicial do objeto e garantir que ele esteja válido antes de usar
+    public Pessoa(String nome, String cpf, int idade, String whatsapp) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.idade = idade;
+        this.whatsapp = whatsapp;
+    }
+
+    public void apresentar() {
+        System.out.println("Meu nome é " + nome +
+                ", sou portador do CPF " + cpf +
+                ", tenho " + idade +
+                " anos e meu zap é " +
+                whatsapp);
+    }
 }
