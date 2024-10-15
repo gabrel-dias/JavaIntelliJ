57c41a479fbd3e4ebe24f2335faa4915bb188bd0
Gabriel Dias <gabrielmdais557@gmail.com>
2024-10-15 01:56:03 -0300

    métodos em poo

--- src/poo/Main.java
+++ src/poo/Main.java
@@ -3,7 +3,14 @@ package poo;
 public class Main {
     public static void main(String[] args) {
         Pessoa p1 = new Pessoa("Gabriel", "075", 24, "085");
+        Pessoa p2 = new Pessoa();
+
         p1.apresentar();
+
+        p2.setNome("Bruna");
+        p2.setIdade(23);
+        p2.setCpf("072");
+        p2.apresentar();
 
     }
 }
--- src/poo/Pessoa.java
+++ src/poo/Pessoa.java
@@ -15,6 +15,33 @@ public class Pessoa {
         this.whatsapp = whatsapp;
     }
 
+    public Pessoa() {
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
     public void apresentar() {
         System.out.println("Meu nome é " + nome +
                 ", sou portador do CPF " + cpf +
