c1eb99ee82656caaff4e506099d78aa9c01668cb
Gabriel Dias <gabrielmdais557@gmail.com>
2024-10-23 01:12:14 -0300

    inputs para leitura de arquivos

--- src/poo/arquivo/Main.java
+++ src/poo/arquivo/Main.java
@@ -1,7 +1,29 @@
 package poo.arquivo;
 
+import java.io.*;
+
 public class Main {
     public static void main(String[] args) {
+        try {
+            InputStream inputStream = new FileInputStream("src/poo/arquivo/livros.csv"); // recebe o caminho de um arquivo que será manipulado
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream); // preparada a estrutura para começar a ler os dados que foram passado pelo InputStream
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader); // manipula os dados
+
+            String linha = bufferedReader.readLine();
+            while (linha != null) {
+                System.out.println(linha);
+                linha = bufferedReader.readLine();
+            }
+
+            inputStream.close();
+            inputStreamReader.close();
+            bufferedReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Arquivo não encontrado!");
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 }
