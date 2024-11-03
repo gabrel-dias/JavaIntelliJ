63f219509890cdfd84a80bbbc711b14eed445b78
Gabriel Dias <gabrielmdais557@gmail.com>
2024-11-03 20:44:51 -0300

    divelamento 3 (fraco, dificulta mais aí)

---
+++ src/exercicios/Nivelamento3.java
@@ -0,0 +1,19 @@
+package exercicios;
+
+import java.util.Scanner;
+
+public class Nivelamento3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a, b, c, d, diferenca;
+
+        a = sc.nextInt();
+        b = sc.nextInt();
+        c = sc.nextInt();
+        d = sc.nextInt();
+        diferenca = (a * b - c * d);
+
+        System.out.println("DIFERENÇA = " + diferenca);
+    }
+}
