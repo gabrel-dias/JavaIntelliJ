09aa7d978bbe2cb01ea9af2159e1195d8c2519a5
Gabriel Dias <gabrielmdais557@gmail.com>
2025-01-28 15:53:50 -0300

    sobrecarga de construtores nos Product.java

--- src/exercicios/udemynelio/exemplos/produto/application/Program.java
+++ src/exercicios/udemynelio/exemplos/produto/application/Program.java
@@ -15,16 +15,16 @@ public class Program {
         String name = sc.nextLine();
         System.out.print("Preço: ");
         double price = sc.nextDouble();
-        System.out.print("Quantidade em estoque: ");
-        int quantity = sc.nextInt();
-        Product product = new Product(name, price, quantity);
+
+        // construtor sem quantidade de estoque
+        Product product = new Product(name, price);
         System.out.println("Dados do produto: " + product);
 
         System.out.println("Digite a quantidade do produto que será atualizada no estoque: ");
-        quantity = sc.nextInt();
+        int quantity = sc.nextInt();
         product.addProducts(quantity);
         System.out.println("Dados do produto atualizados: " + product);
-        System.out.println("Digite a quantidade do produto que será reduzida da estoque: ");
+        System.out.println("Digite a quantidade do produto que será reduzida do estoque: ");
         quantity = sc.nextInt();
         product.removeProducts(quantity);
         System.out.println("Dados do produto atualizados: " + product);
--- src/exercicios/udemynelio/exemplos/produto/entities/Product.java
+++ src/exercicios/udemynelio/exemplos/produto/entities/Product.java
@@ -5,10 +5,9 @@ public class Product {
     public double price;
     public int quantity;
 
-    public Product(String name, double price, int quantity) {
+    public Product(String name, double price) {
         this.name = name;
         this.price = price;
-        this.quantity = quantity;
     }
 
     public double totalValueInStock() {
