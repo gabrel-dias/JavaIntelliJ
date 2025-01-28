a1b140e6ba2346b518cfeb5309a10925e60e3ed0
Gabriel Dias <gabrielmdais557@gmail.com>
2025-01-28 14:46:03 -0300

    uso de construtores no programa de exemplo de produtos

--- src/exercicios/udemynelio/exemplos/produto/application/Program.java
+++ src/exercicios/udemynelio/exemplos/produto/application/Program.java
@@ -10,23 +10,23 @@ public class Program {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
-        Product product = new Product();
         System.out.println("Digite os dados do produto:");
         System.out.print("Nome: ");
-        product.name = sc.nextLine();
+        String name = sc.nextLine();
         System.out.print("Preço: ");
-        product.price = sc.nextDouble();
+        double price = sc.nextDouble();
         System.out.print("Quantidade em estoque: ");
-        product.quantity = sc.nextInt();
+        int quantity = sc.nextInt();
+        Product product = new Product(name, price, quantity);
         System.out.println("Dados do produto: " + product);
 
         System.out.println("Digite a quantidade do produto que será atualizada no estoque: ");
-        int quantidade = sc.nextInt();
-        product.addProducts(quantidade);
+        quantity = sc.nextInt();
+        product.addProducts(quantity);
         System.out.println("Dados do produto atualizados: " + product);
         System.out.println("Digite a quantidade do produto que será reduzida da estoque: ");
-        quantidade = sc.nextInt();
-        product.removeProducts(quantidade);
+        quantity = sc.nextInt();
+        product.removeProducts(quantity);
         System.out.println("Dados do produto atualizados: " + product);
         sc.close();
     }
