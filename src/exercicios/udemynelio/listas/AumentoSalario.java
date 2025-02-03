539aea9f60ca044d44a5ee30f459a635806ce2bd
Gabriel Dias <gabrielmdais557@gmail.com>
2025-02-03 00:29:03 -0300

    desisti do exercicio de lista com lambdas

--- src/exercicios/udemynelio/listas/AumentoSalario.java
+++ src/exercicios/udemynelio/listas/AumentoSalario.java
@@ -70,26 +70,6 @@ class Funcionario {
         return id;
     }
 
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public Double getSalario() {
-        return salario;
-    }
-
-    public void setSalario(Double salario) {
-        this.salario = salario;
-    }
-
     public void aumentarSalario(double porcentagem) {
         salario += salario * porcentagem / 100.0;
     }
