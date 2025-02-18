50a820eaf852f80de10fe9b3da33b9732bbdf3b4
Gabriel Dias <gabrielmdais557@gmail.com>
2025-02-18 00:42:31 -0300

    primeiro exercício de POO da faculdade concluído

--- src/cdl/poo/POO1.java
+++ src/cdl/poo/POO1.java
@@ -1,4 +1,5 @@
-package cdlPOO;
+// GABRIEL MAGALHÃES DIAS - POO - NOITE
+package cdl.poo;
 
 import java.util.Scanner;
 
@@ -8,11 +9,14 @@ public class POO1 {
         Scanner sc = new Scanner(System.in);
         System.out.print("Digite o valor de A: ");
         double a = sc.nextDouble();
+        while (a == 0) {
+            System.out.println("A equação não é de 1º grau, digite um valor menor ou maior que zero. ");
+            a = sc.nextDouble();
+        }
         System.out.print("Digite o valor de B: ");
         double b = sc.nextDouble();
-
         Equacao equacao = new Equacao(a, b);
-        System.out.print("O resultado da equação de 1º grau é: " + equacao.calcularRaiz(a, b));
+        System.out.print("O resultado da equação de 1º grau é: " + equacao.calcularRaiz());
     }
 
 }
@@ -26,7 +30,7 @@ class Equacao {
         this.b = b;
     }
 
-    public double calcularRaiz(double a, double b) {
+    public double calcularRaiz() {
         return -b / a;
     }
 
